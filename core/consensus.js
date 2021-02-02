95e2c7b197807a90dfcb42f98e5fddb775404cbe
CMEONE <contact@kabirwearsato.ga>
2021-02-02 09:31:58 -0800

    Initialize storage and consensus

---
+++ core/consensus.js
@@ -0,0 +1,13 @@
+class FluidityConsensus() {
+	static nextForger(addressData, hash) {
+		
+	}
+	
+	static isValidDrop(drop) {
+		
+	}
+	
+	static isValidWave(wave) {
+		
+	}
+}
---
+++ core/storage.js
@@ -0,0 +1,15 @@
+class FluidityOcean() {
+	
+}
+
+class FluiditySea() {
+	
+}
+
+class FluidityWave() {
+	
+}
+
+class FluidityDrop() {
+	
+}

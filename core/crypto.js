8263b5344bd4a947c384f1985cf5488f5605b31b
CMEONE <contact@kabirwearsato.ga>
2021-02-02 09:28:22 -0800

    Wallet words, keys, address generation

---
+++ core/crypto.js
@@ -0,0 +1,21 @@
+class FluidityCrypto {
+	static generate12Words() {
+		return envoy.randomWords({length: 12});
+	}
+	static generate24Words() {
+		return envoy.randomWords({length: 24});
+	}
+	static getWalletKey(words, nonce) {
+		let nums = envoy.wordsToNumbers({string: words});
+		let key = envoy.genKeys({seed: nonce + ":" + nums, bits: 2048});
+		return key;
+	}
+	static generateWalletAddress(words, nonce) {
+		let key = FluidityCrypto.getWalletKey(words, nonce);
+		let pub = envoy.publicKeyString(key);
+		let hex = envoy.stringToHex(pub);
+		let addr = "0FAx" + hex.substring(hex.length - 46, hex.length - 6);
+		return addr;
+	}
+}
+let envoy = new tEnvoy();

951e275037a867bb6228adb799eb27a7cd0beca7
CMEONE <contact@kabirwearsato.ga>
2021-02-02 09:41:17 -0800

    Basic data storage, more consensus setup

--- core/consensus.js
+++ core/consensus.js
@@ -10,4 +10,8 @@ class FluidityConsensus() {
 	static isValidWave(wave) {
 		
 	}
+	
+	static forgeDrop(drop) {
+		
+	}
 }
--- core/storage.js
+++ core/storage.js
@@ -1,15 +1,35 @@
 class FluidityOcean() {
+	constructor() {
+		this.sea = [];
+	}
 	
+	addSea(sea) {
+		this.seas.push(sea);
+	}
 }
 
 class FluiditySea() {
+	constructor() {
+		this.waves = [];
+	}
 	
+	addWave(wave) {
+		this.waves.push(wave);
+	}
 }
 
 class FluidityWave() {
+	constructor() {
+		this.drops = [];
+	}
 	
+	addDrop(drop) {
+		this.drops.push(drop);
+	}
 }
 
 class FluidityDrop() {
-	
+	constructor() {
+		
+	}
 }
